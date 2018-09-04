e4802c6e560b13cf3ee848ea73e70c73f024cce5
Kyle Dushman <kdushman@indiana.edu>
2018-09-04 16:59:35 -0500

    Initial Commit

---
+++ RedditClient/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  RedditClient
+//
+//  Created by Kyle Dushman on 9/4/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
